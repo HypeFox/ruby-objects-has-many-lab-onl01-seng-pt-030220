ef62b42be29a1267f5c33e396773b55ce686dfd5
Brandon Guffey <hypefox@gmail.com>
2020-04-07 13:28:27 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,16 @@
-class Post 
+class Post
+
+  attr_accessor :title, :author
   
-  attr_accessor :title, :author 
-  
-  def initialize(name)
-    @title = title 
+  def initialize(title)
+    @title = title
   end
-  
+
   def author_name
-    if @author == name
-      return nil 
-    else 
-      @author.name 
+    if @author == nil
+      return nil
+    else
+      @author.name
     end
   end
 end
